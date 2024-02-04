9144052c601a4878dfb85d5192388797a1cd6435
imadeanaccount1 <imadeanaccount@proton.me>
2024-02-04 18:42:15 -0500

    v121.0.0

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -8,7 +8,7 @@ async function screenshot(url: any, width: any, height: any) {
     args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath(
-      `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
+      `https://github.com/Sparticuz/chromium/releases/download/v121.0.0/chromium-v116.0.0-pack.tar`
     ),
     headless: chromium.headless,
     ignoreHTTPSErrors: true,
