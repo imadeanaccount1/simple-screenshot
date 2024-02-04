16dc9074a4cbb6099805aaa962eb511debec439e
imadeanaccount1 <imadeanaccount@proton.me>
2024-02-04 18:37:41 -0500

    hello

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,37 +1,10 @@
-// const fs = require("fs");
+const fs = require("fs");
 import { NextResponse, NextRequest } from "next/server";
-// const chromium = require("@sparticuz/chromium-min");
-// const puppeteer = require("puppeteer");
-
-// async function screenshot(url: any, width: any, height: any) {
-//   const browser = await puppeteer.launch({
-//     args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-//     defaultViewport: chromium.defaultViewport,
-//     executablePath: await chromium.executablePath(
-//       `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
-//     ),
-//     headless: chromium.headless,
-//     ignoreHTTPSErrors: true,
-//   });
-//   console.log("browser");
-//   const pageOne = await browser.newPage();
-//   console.log("page");
-//   await pageOne.goto("https://css.land");
-
-//   const file = await pageOne.screenshot();
-//   browser.close();
-
-//   // await pageOne.waitForTimeout(50000);
-//   return file;
-// }
 const chromium = require("@sparticuz/chromium-min");
 const puppeteer = require("puppeteer");
-let _page: any;
 
-async function getBrowser() {
-  // local development is broken for this 👇
-  // but it works in vercel so I'm not gonna touch it
-  return puppeteer.launch({
+async function screenshot(url: any, width: any, height: any) {
+  const browser = await puppeteer.launch({
     args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath(
@@ -40,27 +13,15 @@ async function getBrowser() {
     headless: chromium.headless,
     ignoreHTTPSErrors: true,
   });
-}
+  console.log("browser");
+  const pageOne = await browser.newPage();
+  console.log("page");
+  await pageOne.goto(url);
 
-async function getPage() {
-  if (_page) return _page;
+  const file = await pageOne.screenshot();
+  browser.close();
 
-  const browser = await getBrowser();
-  _page = await browser.newPage();
-  return _page;
-}
-
-export async function getScreenshot(url: any, ratio = 1) {
-  const page = await getPage();
-  await page.goto(url, {
-    waitUntil: "domcontentloaded",
-  });
-  await page.setViewport({
-    width: 1000,
-    height: 600,
-    devicePixelRatio: ratio,
-  });
-  const file = await page.screenshot();
+  // await pageOne.waitForTimeout(50000);
   return file;
 }
 export async function GET(request: NextRequest) {
@@ -70,7 +31,7 @@ export async function GET(request: NextRequest) {
   const url = searchParams.get("url");
   console.log(width, height, url);
 
-  const file = await getScreenshot(url);
+  const file = await screenshot(url, width, height);
 
   // const image3 = await fs.readFileSync(`./scrapingbee_homepage.jpg`);
 
