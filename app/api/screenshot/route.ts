00019cf1df8afa6f3712fe71493563fd4b6d19b0
imadeanaccount1 <imadeanaccount@proton.me>
2024-02-04 21:43:40 -0500

    local2

--- app/api/screenshot/route.ts
+++ app/api/screenshot/route.ts
@@ -1,23 +1,26 @@
-const fs = require("fs");
+// const fs = require("fs");
 import { NextResponse, NextRequest } from "next/server";
+// const chromium = require("@sparticuz/chromium-min");
 const puppeteer = require("puppeteer");
 
 async function screenshot(url: any, width: any, height: any) {
   const browser = await puppeteer.launch({
-    headless: "new",
     defaultViewport: {
       width: parseInt(width),
       height: parseInt(height),
     },
+    headless: "new",
   });
+  console.log("browser");
   const pageOne = await browser.newPage();
+  console.log("page");
   await pageOne.goto(url);
 
-  await pageOne.screenshot({ path: `./scrapingbee_homepage.jpg` });
+  const file = await pageOne.screenshot();
+  browser.close();
 
   // await pageOne.waitForTimeout(50000);
-  browser.close();
-  return null;
+  return file;
 }
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
@@ -26,11 +29,11 @@ export async function GET(request: NextRequest) {
   const url = searchParams.get("url");
   console.log(width, height, url);
 
-  await screenshot(url, width, height);
+  const file = await screenshot(url, width, height);
 
-  const image3 = await fs.readFileSync(`./scrapingbee_homepage.jpg`);
+  // const image3 = await fs.readFileSync(`./scrapingbee_homepage.jpg`);
 
-  const response = new NextResponse(image3, {
+  const response = new NextResponse(file, {
     headers: {
       "content-type": "image/jpg",
     },
