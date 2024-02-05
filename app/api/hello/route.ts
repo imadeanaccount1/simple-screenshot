f3e5e908b00ea89443db5cb192c58dbb1ea4defc
imadeanaccount1 <imadeanaccount@proton.me>
2024-02-04 19:04:39 -0500

    width and height

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -6,7 +6,10 @@ const puppeteer = require("puppeteer");
 async function screenshot(url: any, width: any, height: any) {
   const browser = await puppeteer.launch({
     args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-    defaultViewport: chromium.defaultViewport,
+    defaultViewport: {
+      width: parseInt(width),
+      height: parseInt(height),
+    },
     executablePath: await chromium.executablePath(
       `https://github.com/Sparticuz/chromium/releases/download/v121.0.0/chromium-v121.0.0-pack.tar`
     ),
