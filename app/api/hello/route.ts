9320578dc5b364a642c1e5f274ffd7de46684115
imadeanaccount1 <imadeanaccount@proton.me>
2024-02-04 19:00:33 -0500

    test

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -5,12 +5,12 @@ const puppeteer = require("puppeteer");
 
 async function screenshot(url: any, width: any, height: any) {
   const browser = await puppeteer.launch({
-    args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+    // args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath(
       `https://github.com/Sparticuz/chromium/releases/download/v121.0.0/chromium-v116.0.0-pack.tar`
     ),
-    headless: chromium.headless,
+    headless: true,
     ignoreHTTPSErrors: true,
   });
   console.log("browser");
